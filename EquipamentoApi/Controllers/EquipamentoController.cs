fc022c158fc5ffdce0907fe15dd3fc6583795cb2
Victor <victor.liger0800@gmail.com>
2024-06-09 01:37:34 -0300

    Refatorando projeto com dapper de ORM, string de conexão funcionando e primeiro método http esta OK

---
+++ EquipamentoApi/Controllers/EquipamentoController.cs
@@ -0,0 +1,56 @@
+﻿using Dapper;
+using EquipamentoApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+using System.Linq.Expressions;
+
+namespace EquipamentoApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EquipamentoController : ControllerBase
+    {
+
+        public readonly string _connectionString;
+        public EquipamentoController(IConfiguration config)
+        {
+            _connectionString = config.GetConnectionString("Default");
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllEquipaments()
+        {
+            try
+            {
+                using (var conn = new NpgsqlConnection(_connectionString))
+                {
+                    conn.Open();
+
+                    const string query = "SELECT * FROM equipamentos";
+
+                    var equipaments = await conn.QueryAsync<EquipamentoModel>(query);
+
+                    if (equipaments is null || equipaments.Count().Equals(0))
+                        throw new InvalidOperationException("Não achamos nenhum equipamento");
+
+                    return Ok(equipaments);
+
+                }
+            }
+            catch (InvalidOperationException ex)
+            {
+                return NotFound(ex.Message);
+
+            }
+            catch (NpgsqlException ex)
+            {
+                return BadRequest("Ocorreu um erro ao acessar ro banco de dados");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ EquipamentoApi/Models/EquipamentoModel.cs
@@ -0,0 +1,14 @@
+﻿namespace EquipamentoApi.Models
+{
+    public class EquipamentoModel
+    {
+        protected EquipamentoModel() { }
+
+        public string Tag { get; private set; }
+        public string Name { get; private set; }
+        public string File { get; private set; }
+        public string State { get; private set; }
+        public DateTime Data { get; private set; }
+
+    }
+}
